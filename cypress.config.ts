3cce92c2ce93420a4632694b2c0995045f996cbb
Yuriy <yuramax@gmail.com>
2024-10-31 22:22:56 +0200

    add cypress config file; adding files with tests

---
+++ cypress.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'cypress'
+import { addMatchImageSnapshotPlugin } from '@simonsmith/cypress-image-snapshot/plugin'
+
+export default defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  screenshotsFolder: 'cypress/screenshotsFolder',
+  videosFolder: 'cypress/videosFolder',
+  pageLoadTimeout: 150000,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      addMatchImageSnapshotPlugin(on)
+
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'electron' && browser.isHeadless) {
+          launchOptions.preferences.width = 1920;
+          launchOptions.preferences.height = 1080;
+        }
+
+        return launchOptions;
+      });
+    },
+    baseUrl: 'http://localhost:3000/uk',
+    video: false,
+  },
+})
---
+++ cypress/e2e/blog_page.cy.ts
@@ -0,0 +1,47 @@
+const viewPorts = [
+  { name: 'iphone-8', dimensions: [375, 667] },
+  { name: 'desktop', dimensions: [1920, 1080] },
+];
+
+describe('Blog Page Tests', () => {
+  it('Visit to kavoon.com.ua/uk/blog', () => {
+    cy.visit('http://localhost:3000/uk/');
+    cy.get('[data-cy="blog-footer-link"]').should('have.text', 'Блог').click();
+    cy.url().should('include', 'uk/blog');
+  });
+
+  it('Visit to kavoon.com.ua/en/blog', () => {
+    cy.visit('http://localhost:3000/en/');
+    cy.get('[data-cy="blog-footer-link"]').should('have.text', 'Blog').click();
+    cy.url().should('include', 'en/blog');
+  });
+
+  it('Visit to kavoon.com.ua/pl/blog', () => {
+    cy.visit('http://localhost:3000/pl/');
+    cy.get('[data-cy="blog-footer-link"]').should('have.text', 'Blog').click();
+    cy.url().should('include', 'pl/blog');
+  });
+
+  beforeEach(() => {
+    cy.visit('/blog');
+  });
+
+  viewPorts.forEach(({ name, dimensions }) => {
+    const [width, height] = dimensions;
+
+    it(`Should display correctly on ${name} viewport`, () => {
+      cy.viewport(width, height);
+      cy.get('[data-cy="blog-page"]').should('be.visible');
+      cy.scrollTo('bottom', { duration: 3000 });
+
+      cy.get('[data-cy="header"]').invoke('css', 'position', 'absolute');
+
+      cy.waitForNetworkIdle(5000);
+      cy.matchImageSnapshot(`blog-page-${name}`);
+
+      cy.get('[data-cy="header"]').invoke('css', 'position', 'fixed');
+    });
+  });
+});
+
+export {};
