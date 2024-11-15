3437d1c848b641133f245fc8355f2640920480e5
Vitalii Pikozh <wisart@gmail.com>
2024-11-15 11:47:38 +0200

    fix howDiscoverPicker

--- src/helpers/makeMessengersList.ts
+++ src/helpers/makeMessengersList.ts
@@ -1,4 +1,4 @@
-import lang from 'src/i18n/lang';
+import translate from 'src/i18n/lang';
 import { Language } from 'src/types/language';
 import { Messenger } from 'src/types/pickersProps';
 
@@ -7,17 +7,17 @@ function MakeMessengersList(language: Language): Messenger[] {
     {
       name: 'Telegram',
       src: '/icons/telegram-icon.svg',
-      placeholder: lang('UsernameOrPhoneNumber', language),
+      placeholder: translate('UsernameOrPhoneNumber', language),
     },
     {
       name: 'Viber',
       src: '/icons/viber-icon.svg',
-      placeholder: lang('UsernameOrPhoneNumber', language),
+      placeholder: translate('UsernameOrPhoneNumber', language),
     },
     {
       name: 'Whatsapp',
       src: '/icons/whatsapp-icon.svg',
-      placeholder: lang('PhoneNumber', language),
+      placeholder: translate('PhoneNumber', language),
     },
   ];
 }
