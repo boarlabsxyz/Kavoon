db2b54e81073ba908cc2a097daa9ccfa65806e15
IhorMasechko <igor.masechko007@gmail.com>
2024-11-19 10:44:47 +0200

    Fixed the highlight issue on the 'Cart' page.

--- src/components/common/header/Navigation/NavMain.tsx
+++ src/components/common/header/Navigation/NavMain.tsx
@@ -22,40 +22,31 @@ const pages = [
 function NavMain({ language }: NavMainProps) {
   const pathname = usePathname();
   const pathWithoutLocale = pathname.substring(4);
-
-  const isActivePath = (target: string) => {
-    if (target === 'shop') {
-      if (
-        pathWithoutLocale === 'shop/bicycle-equipment' ||
-        pathWithoutLocale === 'shop/bag-accessories' ||
-        pathWithoutLocale === 'shop/city-collection' ||
-        pathWithoutLocale === 'shop/in-stock'
-      ) {
-        return true;
-      }
-      return false;
-    }
-    return pathWithoutLocale === target;
-  };
+  const pathSegments = pathWithoutLocale.split('/');
 
   return (
     <nav className={styles.navMain}>
       <ul className={styles.list}>
-        {pages.map(({ target, label }) => (
-          <li key={target} data-cy={`${target}-header-link`}>
-            <Link
-              className={
-                isActivePath(target)
-                  ? `${styles.link} ${styles.active}`
-                  : styles.link
-              }
-              href={`/${language}/${target}`}
-              prefetch={false}
-            >
-              {lang(label, language)}
-            </Link>
-          </li>
-        ))}
+        {pages.map(({ target, label }) => {
+          const isTargetActive =
+            pathSegments[0] === target && pathWithoutLocale !== 'shop/cart';
+
+          return (
+            <li key={target} data-cy={`${target}-header-link`}>
+              <Link
+                className={
+                  isTargetActive
+                    ? `${styles.link} ${styles.active}`
+                    : styles.link
+                }
+                href={`/${language}/${target}`}
+                prefetch={false}
+              >
+                {lang(label, language)}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
       <InformationBlock lang={language} />
     </nav>
