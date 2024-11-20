e928c7c2abf381e8bc3ce4c7fe6a4b2d7abcaf1d
Vitalii Pikozh <wisart@gmail.com>
2024-11-20 20:27:37 +0200

    fix eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,131 +1,153 @@
-import react from "eslint-plugin-react";
-import prettier from "eslint-plugin-prettier";
-import cypress from "eslint-plugin-cypress";
-import globals from "globals";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+import cypress from 'eslint-plugin-cypress';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
     ignores: [
-        "**/node_modules/",
-        "**/.vscode/",
-        "**/.next/",
-        "**/.serverless/",
-        "**/*.md",
-        "**/coverage/",
-        "**/components",
+      '**/node_modules/',
+      '**/.vscode/',
+      '**/.next/',
+      '**/.serverless/',
+      '**/*.md',
+      '**/coverage/',
+      '**/components',
     ],
-}, ...compat.extends(
-    "plugin:react/recommended",
-    "airbnb",
-    "next",
-    "next/core-web-vitals",
-    "plugin:cypress/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-), {
+  },
+  ...compat.extends(
+    'plugin:react/recommended',
+    'airbnb',
+    'next',
+    'next/core-web-vitals',
+    'plugin:cypress/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ),
+  {
     plugins: {
-        react,
-        prettier,
-        cypress,
+      react,
+      prettier,
+      cypress,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            Atomics: "readonly",
-            SharedArrayBuffer: "readonly",
-        },
+      globals: {
+        ...globals.browser,
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+      },
 
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
 
-        parserOptions: {
-            ecmaFeatures: {
-                jsx: true,
-            },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
+      },
     },
 
     rules: {
-        "click-events-have-key-events": "off",
-        "prettier/prettier": "error",
+      'click-events-have-key-events': 'off',
+      'prettier/prettier': 'error',
 
-        "react/jsx-filename-extension": [1, {
-            extensions: [".js", ".jsx", ".tsx", ".mdx"],
-        }],
+      'react/jsx-filename-extension': [
+        1,
+        {
+          extensions: ['.js', '.jsx', '.tsx', '.mdx'],
+        },
+      ],
 
-        "react/jsx-one-expression-per-line": 0,
-        "react/prop-types": 0,
-        "jsx-a11y/click-events-have-key-events": 0,
+      'react/jsx-one-expression-per-line': 0,
+      'react/prop-types': 0,
+      'jsx-a11y/click-events-have-key-events': 0,
 
-        "jsx-a11y/label-has-associated-control": ["error", {
-            required: {
-                some: ["nesting", "id"],
-            },
-        }],
+      'jsx-a11y/label-has-associated-control': [
+        'error',
+        {
+          required: {
+            some: ['nesting', 'id'],
+          },
+        },
+      ],
 
-        "jsx-a11y/no-noninteractive-element-interactions": 0,
-        "react/jsx-props-no-spreading": 0,
+      'jsx-a11y/no-noninteractive-element-interactions': 0,
+      'react/jsx-props-no-spreading': 0,
 
-        "no-param-reassign": ["error", {
-            props: false,
-        }],
+      'no-param-reassign': [
+        'error',
+        {
+          props: false,
+        },
+      ],
 
-        "jsx-a11y/anchor-is-valid": 0,
+      'jsx-a11y/anchor-is-valid': 0,
 
-        "no-unused-expressions": ["error", {
-            allowShortCircuit: true,
-            allowTernary: true,
-        }],
+      'no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+        },
+      ],
 
-        "jsx-a11y/control-has-associated-label": 0,
-        "jsx-a11y/no-static-element-interactions": 0,
-        "import/extensions": "off",
-        "react/function-component-definition": "off",
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/ban-types": "off",
-        "no-promise-executor-return": "off",
-        "@typescript-eslint/no-var-requires": "off",
-        "no-restricted-exports": "off",
-        "react/require-default-props": "off",
-        "@typescript-eslint/no-unused-vars": "off",
-        "react/jsx-no-useless-fragment": "off",
-        "import/no-extraneous-dependencies": "off",
-        "global-require": "off",
-        "@typescript-eslint/no-empty-function": "off",
-        "react/no-unstable-nested-components": "off",
+      'jsx-a11y/control-has-associated-label': 0,
+      'jsx-a11y/no-static-element-interactions': 0,
+      'import/extensions': 'off',
+      'react/function-component-definition': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      'no-promise-executor-return': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      'no-restricted-exports': 'off',
+      'react/require-default-props': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'react/jsx-no-useless-fragment': 'off',
+      'import/no-extraneous-dependencies': 'off',
+      'global-require': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      'react/no-unstable-nested-components': 'off',
 
-        "no-underscore-dangle": ["error", {
-            allow: ["_id"],
-        }],
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['_id'],
+        },
+      ],
     },
-}, ...compat.extends("plugin:mdx/recommended").map(config => ({
+  },
+  ...compat.extends('plugin:mdx/recommended').map((config) => ({
     ...config,
-    files: ["**/*.mdx"],
-})), {
-    files: ["**/*.mdx"],
+    files: ['**/*.mdx'],
+  })),
+  {
+    files: ['**/*.mdx'],
 
     settings: {
-        "mdx/code-blocks": true,
-        "mdx/no-unused-expressions": true,
-        "mdx/no-jsx-html-comments": false,
+      'mdx/code-blocks': true,
+      'mdx/no-unused-expressions': true,
+      'mdx/no-jsx-html-comments': false,
     },
 
     rules: {
-        "react/jsx-no-undef": "off",
-        "react/self-closing-comp": "off",
+      'react/jsx-no-undef': 'off',
+      'react/self-closing-comp': 'off',
     },
-}];
+  },
+];
