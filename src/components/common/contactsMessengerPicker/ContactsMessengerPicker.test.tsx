bb703de6b97db65f072590d9507959c3eafa4aeb
Vitalii Pikozh <wisart@gmail.com>
2024-11-21 18:46:50 +0200

    fix ContactsMessengerPicker and test for it (#57)
    - add read-only mark to the ContactsMessengerPicker props;
    - set remarkText prop in the ContactsMessengerPicker as boolean;
    - set some props in the ContactsMessengerPicker as optional;
    - fix ContactsMessengerPicker.test.

--- src/components/common/contactsMessengerPicker/ContactsMessengerPicker.test.tsx
+++ src/components/common/contactsMessengerPicker/ContactsMessengerPicker.test.tsx
@@ -34,7 +34,6 @@ describe('ContactsMessengerPicker Component', () => {
                 value: values.numTel,
               }}
               language={language}
-              remarkText=""
             />
           )}
         </Formik>
