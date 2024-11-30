36dbcb1ba40548fe2994a92210436d6e3349883a
Vitalii Pikozh <wisart@gmail.com>
2024-11-30 13:38:23 +0200

    Revert "Added a loading spinner to the shop page and the about-us page (#73)"
    This reverts commit 1e45a02cd774736fdfa45f4f43f68c01c5b7617c.

--- src/app/[lang]/loading.tsx
+++
@@ -1,7 +0,0 @@
-import Spinner from 'src/components/common/spinner';
-
-function Loader() {
-  return <Spinner />;
-}
-
-export default Loader;
--- src/components/common/spinner/spinner.tsx
+++ src/components/common/spinner/spinner.tsx
@@ -1,7 +1,6 @@
 import { FC, CSSProperties } from 'react';
 
 import BeatLoader from 'react-spinners/BeatLoader';
-import st from './loading.module.css';
 
 interface IProps {
   loading?: boolean;
@@ -16,21 +15,14 @@ const override: CSSProperties = {
 };
 
 const Spinner: FC<IProps> = ({ loading = true, color = '#11a0a9' }) => (
-  <div
-    className={st.wrapper}
-    role="status"
-    aria-live="polite"
-    aria-label="Loading"
-  >
-    <BeatLoader
-      color={color}
-      loading={loading}
-      cssOverride={override}
-      size={22}
-      margin={8}
-      speedMultiplier={0.7}
-    />
-  </div>
+  <BeatLoader
+    color={color}
+    loading={loading}
+    cssOverride={override}
+    size={22}
+    margin={8}
+    speedMultiplier={0.7}
+  />
 );
 
 export default Spinner;
