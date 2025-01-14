08d8e72fb4398a7b85b69adc325e46fc0182a6a8
Anton Kovalchuck <antonkovalchuck@gmail.com>
2025-01-14 12:04:49 +0200

    removed console.logs

--- src/helpers/getPathWithoutLocale.ts
+++ src/helpers/getPathWithoutLocale.ts
@@ -3,9 +3,7 @@ export const getPathWithoutLocale = (pathname: string): string => {
     throw new Error('Invalid pathname');
   }
   const segments = pathname.split('/');
-  console.log('segments', segments);
   if (segments.length < 3) {
-    console.log('inside segments.length < 3');
     return '';
   }
   return pathname.split('/').slice(2).join('/');
