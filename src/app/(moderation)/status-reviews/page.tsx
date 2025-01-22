ee7d49adda87a6c22b977de572604ab8a0eccdc5
Vitalii Pikozh <wisart@gmail.com>
2025-01-22 12:38:01 +0200

    test

---
+++ src/app/(moderation)/status-reviews/page.tsx
@@ -0,0 +1,17 @@
+import StatusReviews from 'src/components/statusReviewsPage';
+
+async function StatusReviewsPage() {
+  // const apiBaseUrl =
+  //   process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000';
+
+  // const response = await fetch(`${apiBaseUrl}/api/reviews`);
+  const response = await fetch('http://localhost:3000/api/reviews');
+  if (!response.ok) {
+    throw new Error('Failed to fetch reviews');
+  }
+
+  const data = await response.json();
+  return <StatusReviews reviews={data.data} />;
+}
+
+export default StatusReviewsPage;
