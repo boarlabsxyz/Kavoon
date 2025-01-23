b01276a1272899019e51b47d4776a3caa841e65a
Vitalii Pikozh <wisart@gmail.com>
2025-01-23 20:49:57 +0200

    hide cartButton on cart page

--- src/components/common/cartButton/cartButton.tsx
+++ src/components/common/cartButton/cartButton.tsx
@@ -5,12 +5,23 @@ import CartStatusVM from 'src/data/viewModels/cart/cartStatusVM';
 import { Language } from 'src/types/language';
 
 import st from './cartButton.module.css';
+import { usePathname } from 'next/navigation';
+import { getPathWithoutLocale } from 'src/helpers/getPathWithoutLocale';
 
 type Props = {
   language: Language;
 };
 
 function CartButton({ language }: Props) {
+  const pathname = usePathname();
+  const pathWithoutLocale = getPathWithoutLocale(pathname);
+  const pathSegments = pathWithoutLocale.split('/');
+  const isCartPage = pathSegments.at(-1) === 'cart';
+
+  if (isCartPage) {
+    return null;
+  }
+
   return (
     <div className={st.wrapper}>
       <CartItemCount vm={new CartStatusVM()} language={language} />
