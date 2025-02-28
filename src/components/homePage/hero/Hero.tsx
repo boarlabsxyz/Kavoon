befa92030fc9759b5bfb3d6a29e253695b4d4f72
IhorMasechko <igor.masechko007@gmail.com>
2025-02-28 13:17:11 +0200

    Deleted background-image for testting

--- src/components/homePage/hero/Hero.tsx
+++ src/components/homePage/hero/Hero.tsx
@@ -1,13 +1,13 @@
 import { memo } from 'react';
 import dynamic from 'next/dynamic';
 import Container from 'src/components/common/container';
-// import Title from 'src/components/homePage/title';
+import Title from 'src/components/homePage/title';
 import st from './Hero.module.css';
 import { Language } from 'src/types/language';
 
-// const Subtitle = dynamic(() => import('src/components/homePage/subtitle'), {
-//   ssr: true,
-// });
+const Subtitle = dynamic(() => import('src/components/homePage/subtitle'), {
+  ssr: true,
+});
 
 type HeroProps = {
   lang: Language;
@@ -18,8 +18,8 @@ const Hero = ({ lang }: HeroProps) => {
     <section data-testid="hero" className={st.hero}>
       <Container>
         <div className={st.wrapper}>
-          {/* <Title />
-          <Subtitle language={lang} /> */}
+          <Title />
+          <Subtitle language={lang} />
         </div>
       </Container>
     </section>
