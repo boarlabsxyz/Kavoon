2c51ffae7bebc1e695229bb2914847d9ee9d0403
Yuriy <yuramax@gmail.com>
2025-03-05 18:23:03 +0200

    creating global-error.tsx file to catch errors from Sentry

---
+++ src/app/global-error.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import * as Sentry from '@sentry/nextjs';
+import { useEffect } from 'react';
+
+export default function GlobalError({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
+
+  return (
+    <html>
+      <body>
+        <h1>Something went wrong!</h1>
+        <button onClick={() => reset()}>Try one more time</button>
+      </body>
+    </html>
+  );
+}
