ff81a43fee6567f534076486cfebddeeae0c951a
Yuriy <yuramax@gmail.com>
2025-03-11 09:39:50 +0200

    canonical URL for Delivery and Payment page

--- src/app/[lang]/blog/page.test.tsx
+++ src/app/[lang]/blog/page.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Blog, { generateMetadata } from './page';
 import { getPosts } from 'src/services/blog';
