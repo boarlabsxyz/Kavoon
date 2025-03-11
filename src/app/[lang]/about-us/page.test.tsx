9b7e6e453e639c59c37a80e586b6c1287b649e12
Yuriy <yuramax@gmail.com>
2025-03-11 09:18:37 +0200

    jest tests for About Us page

---
+++ src/app/[lang]/about-us/page.test.tsx
@@ -0,0 +1,66 @@
+import { act, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import AboutUsPage, { generateMetadata } from './page';
+import getDictionary from 'src/i18n/getDictionary';
+import { Language } from 'src/types/language';
+import { renderToString } from 'react-dom/server';
+
+jest.mock('src/components/aboutUsPage/aboutUs/AboutUs', () => () => (
+  <div data-testid="about-us-component" />
+));
+
+jest.mock('src/i18n/getDictionary', () => jest.fn());
+
+describe('AboutUsPage Component (Server-side)', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders AboutUs component when dictionary loads successfully', async () => {
+    (getDictionary as jest.Mock).mockResolvedValue({
+      title: 'Про нас',
+      subtitle: 'Дізнайтеся більше про Kavoon',
+    });
+
+    let html: string;
+
+    await act(async () => {
+      html = renderToString(
+        await AboutUsPage({ params: { lang: 'en' as Language } })
+      );
+    });
+
+    expect(html).toContain('data-testid="about-us-component"');
+  });
+
+  it('displays error message when dictionary fails to load', async () => {
+    (getDictionary as jest.Mock).mockRejectedValue(
+      new Error('Failed to fetch dictionary')
+    );
+
+    let html: string;
+
+    await act(async () => {
+      html = renderToString(
+        await AboutUsPage({ params: { lang: 'en' as Language } })
+      );
+    });
+
+    expect(html).toContain('Error loading About Us page.');
+  });
+});
+
+describe('generateMetadata', () => {
+  it('generates correct metadata', () => {
+    const metadata = generateMetadata({ params: { lang: 'en' as Language } });
+
+    expect(metadata.alternates?.canonical).toBe(
+      'https://kavoon.com.ua/en/about-us'
+    );
+    expect(metadata.alternates?.languages).toEqual({
+      en: 'https://kavoon.com.ua/en/about-us',
+      uk: 'https://kavoon.com.ua/uk/about-us',
+      pl: 'https://kavoon.com.ua/pl/about-us',
+    });
+  });
+});
--- src/app/[lang]/about-us/page.tsx
+++ src/app/[lang]/about-us/page.tsx
@@ -1,4 +1,5 @@
 import AboutUs from 'src/components/aboutUsPage/aboutUs/AboutUs';
+import { Metadata } from 'next';
 
 import getDictionary from 'src/i18n/getDictionary';
 import { I18N } from 'src/types/i18n.type';
@@ -9,12 +10,31 @@ type Props = {
   params: { lang: Language };
 };
 
+export function generateMetadata({ params }: Props): Metadata {
+  const baseUrl = 'https://kavoon.com.ua';
+  const { lang } = params;
+  const path = '/about-us';
+
+  return {
+    // title: 'Про нас | Kavoon',
+    // description: 'Дізнайтеся більше про Kavoon',
+    alternates: {
+      canonical: `${baseUrl}/${lang}${path}`,
+      languages: {
+        en: `${baseUrl}/en${path}`,
+        uk: `${baseUrl}/uk${path}`,
+        pl: `${baseUrl}/pl${path}`,
+      },
+    },
+  };
+}
+
 async function AboutUsPage({ params: { lang } }: Props) {
   try {
     const i18n = (await getDictionary(lang, 'aboutUs')) as I18N;
     return <AboutUs dictionary={i18n} />;
   } catch (error) {
-    return <div>Error loading policy page.</div>;
+    return <div>Error loading About Us page.</div>;
   }
 }
 
